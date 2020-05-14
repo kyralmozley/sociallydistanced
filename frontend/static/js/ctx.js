7f32f36400c40dd3a75aa71b640a8dc9a660786f
Elliott Mozley <32818082+coexperience@users.noreply.github.com>
2020-05-14 16:46:00 +0100

    Frontend: Implement daily forecast chart

--- frontend/static/js/ctx.js
+++ frontend/static/js/ctx.js
@@ -3,6 +3,15 @@
  * @todo finish this -- it's not really done yet.
  */
 ;(function () {
+	// Copied from scss/core/_variables.scss
+	var colours = {
+		red: "#e14a4e",
+		orange: "#ee8632",
+		yellow: "#f7c117",
+		lime: "#9cbd38",
+		green: "#4fb373",
+	}
+
 	var labels = []
 	for (var i = 0; i < 24; i++) {
 		labels.push(i + ":00")
@@ -11,20 +20,55 @@
 	function renderForecast(points) {
 		var canvas = $(".result-graph")
 
-		/*
-		var myChart = new Chart(canvas, {
-			type: "line",
-			data: {
-				datasets: [
-					{
-						label: "Prediction",
-						data: points,
-						borderWidth: 1,
-					},
-				],
-			},
-		})
-		*/
+		var chart_colours = []
+		for (i in points) {
+			points[i]++
+			switch (points[i]) {
+				case 1: {
+					chart_colours[i] = colours.green
+					break
+				}
+
+				case 2: {
+					chart_colours[i] = colours.lime
+					break
+				}
+
+				case 3: {
+					chart_colours[i] = colours.yellow
+					break
+				}
+				case 4: {
+					chart_colours[i] = colours.yellow
+					break
+				}
+				case 5: {
+					chart_colours[i] = colours.yellow
+					break
+				}
+
+				case 6: {
+					chart_colours[i] = colours.orange
+					break
+				}
+				case 7: {
+					chart_colours[i] = colours.orange
+					break
+				}
+
+				case 8: {
+					chart_colours[i] = colours.red
+					break
+				}
+				case 9: {
+					chart_colours[i] = colours.red
+					break
+				}
+			}
+		}
+
+		console.log("Chart colours", chart_colours)
+		console.log("Points", points)
 
 		var myChart = new Chart(canvas, {
 			type: "bar",
@@ -33,47 +77,37 @@
 				datasets: [
 					{
 						label: "Prediction",
+						backgroundColor: chart_colours,
 						data: points,
-						barPercentage: 1,
-						backgroundColor: [
-							"rgba(255, 99, 132, 1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 01)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderColor: [
-							"rgba(255, 99, 132, 1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
 					},
 				],
 			},
 			options: {
-				scaleOverride: true,
-				scaleSteps: 10,
-				scaleStepWidth: 50,
-				scaleStartValue: 0,
-
-				responsive: true,
-				maintainAspectRatio: true,
 				legend: {
 					display: false,
 				},
 				scales: {
-					yAxes: [
-						{
-							ticks: {
-								max: 5,
+					y: {
+						min: 0,
+						max: 9,
+						stepSize: 1,
+						display: false,
+					},
+					yAxes: {
+						gridLines: {
+							display: false,
+						},
+						ticks: {
+							callback: function (value) {
+								return (value * 100).toFixed(0) + "%" // convert it to percentage
 							},
 						},
-					],
+					},
+					xAxes: {
+						gridLines: {
+							display: false,
+						},
+					},
 				},
 			},
 		})
