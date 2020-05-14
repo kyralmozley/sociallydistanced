6519b84d24538853d1fc6dc736407e09cff048cb
Kyra <kyralmozley@outlook.com>
2020-05-14 16:50:18 +0100

    Return ranking 1-10

--- ml/prediction.py
+++ ml/prediction.py
@@ -14,7 +14,6 @@ def makePrediction(placeID):
     [lat, long] = getData.googlePlaceIDtoLatLong(placeID)
     weather_ranking = getData.getWeather(lat, long)
     [temps, forecast, chance_rain] = weather_ranking
-
     combine = [a * b for a, b in zip(google_ranking[0], temps)]
     combine = [a * b for a, b in zip(combine, forecast)]
     combine = [a * b for a, b in zip(combine, chance_rain)]
@@ -38,16 +37,4 @@ def getCurrentPrediction():
 
 
 def getDayForecast():
-    forecast = []
-    for item in day_forecast:
-        if item > 40:
-            forecast.append(4)
-        elif item > 30:
-            forecast.append(3)
-        elif item > 20:
-            forecast.append(2)
-        elif item > 10:
-            forecast.append(1)
-        else:
-            forecast.append(0)
-    return forecast
+    return [i * 1.5 for i in day_forecast]
